1c492303fe26de7e566756fe6884de74da80e408
pakash2003pramanick <prakashpramanickjsr1717@gmail.com>
2025-03-26 23:21:35 +0530

    google authentication

--- controllers/fetchCourses.js
+++ controllers/fetchCourses.js
@@ -1,1 +1,21 @@
 //fetchCourses
+// to keep in form while creating an assignment 
+const axios = require('axios');
+const COURSES_URL = 'https://classroom.googleapis.com/v1/courses';
+const fetchCourses = async (req, res) => {
+    const { access_token } = req.user.google;
+    try {
+        const coursesResponse = await axios.get(COURSES_URL, {
+            headers: {
+                Authorization: `Bearer ${access_token}`,
+            },
+        });
+        console.log("Courses Response", coursesResponse.data);
+        const courses = coursesResponse.data.courses;
+        res.status(200).json({ courses });
+
+    } catch (error) {
+        console.error("Error fetching courses", error.response?.data || error.message);
+    }
+}
+module.exports = { fetchCourses };
--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  google: { type: mongoose.Schema.Types.Mixed, default: null},
 }, { timestamps: true });
 
 // Create User Model
