27b184954f2bc78c66e8bf6a6e3ff24d49d27f8b
pakash2003pramanick <prakashpramanickjsr1717@gmail.com>
2025-04-08 00:25:52 +0530

    updated login

--- controllers/auth/google/getAccessToken.js
+++ controllers/auth/google/getAccessToken.js
@@ -66,6 +66,9 @@ const getAccessToken = async (req, res) => {
     // Build URL with token and serialized user object
     const finalRedirectUrl = `${redirectUrl}?token=${jwtToken}&user=${userString}`;
 
+
+    console.log("Final Redirect URL", finalRedirectUrl);
+
     // Redirect to frontend with query params
     return res.redirect(finalRedirectUrl);
   } catch (error) {
